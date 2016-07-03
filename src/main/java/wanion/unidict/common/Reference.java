ac46fefca374ca18f55d82ad5b76e55e696824a7
WanionCane <wanioncane@gmail.com>
2016-07-03 11:23:45 -0300

    following 1.9.4²

--- src/main/java/wanion/unidict/common/Reference.java
+++ src/main/java/wanion/unidict/common/Reference.java
@@ -14,7 +14,7 @@ public final class Reference
 {
     public static final String MOD_ID = "UniDict";
     public static final String MOD_NAME = MOD_ID;
-    public static final String MOD_VERSION = "1.7.10-2.7";
+    public static final String MOD_VERSION = "1.7.10-2.7b";
     public static final char SLASH = separatorChar;
     public static final String MC_VERSION = "[1.7.10]";
 
