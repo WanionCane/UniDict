d85ee74a194e9fb176cc1e2ee9bbb02523b60508
WanionCane <wanioncane@gmail.com>
2016-09-03 13:04:40 -0300

    New: Input Replacement

--- src/main/java/wanion/unidict/integration/FurnaceIntegration.java
+++ src/main/java/wanion/unidict/integration/FurnaceIntegration.java
@@ -8,10 +8,17 @@ package wanion.unidict.integration;
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import gnu.trove.set.TIntSet;
+import gnu.trove.set.hash.TIntHashSet;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
+import wanion.unidict.Config;
 import wanion.unidict.UniDict;
+import wanion.unidict.resource.UniResourceContainer;
 
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 final class FurnaceIntegration extends AbstractIntegrationThread
@@ -32,7 +39,34 @@ final class FurnaceIntegration extends AbstractIntegrationThread
 
     private void optimizeFurnaceRecipes()
     {
-        for (final Map.Entry<ItemStack, ItemStack> furnaceRecipe : FurnaceRecipes.instance().getSmeltingList().entrySet())
-            furnaceRecipe.setValue(resourceHandler.getMainItemStack(furnaceRecipe.getValue()));
+        if (!(Config.keepOneEntry || Config.inputReplacement))
+            for (final Map.Entry<ItemStack, ItemStack> furnaceRecipe : FurnaceRecipes.instance().getSmeltingList().entrySet())
+                furnaceRecipe.setValue(resourceHandler.getMainItemStack(furnaceRecipe.getValue()));
+        else {
+            final Map<UniResourceContainer, TIntSet> containerKindMap = new IdentityHashMap<>();
+            final Map<ItemStack, ItemStack> furnaceRecipes = FurnaceRecipes.instance().getSmeltingList();
+            final Map<ItemStack, ItemStack> newRecipes = new HashMap<>();
+            for (final Iterator<Map.Entry<ItemStack, ItemStack>> furnaceRecipeIterator = furnaceRecipes.entrySet().iterator(); furnaceRecipeIterator.hasNext(); )
+            {
+                final Map.Entry<ItemStack, ItemStack> furnaceRecipe = furnaceRecipeIterator.next();
+                final UniResourceContainer inputContainer = resourceHandler.getContainer(furnaceRecipe.getKey());
+                final UniResourceContainer outputContainer = resourceHandler.getContainer(furnaceRecipe.getValue());
+                if (outputContainer != null && inputContainer == null) {
+                    furnaceRecipe.setValue(outputContainer.getMainEntry(furnaceRecipe.getValue().stackSize));
+                    continue;
+                } else if (outputContainer == null)
+                    continue;
+                final int kind = inputContainer.kind;
+                if (!containerKindMap.containsKey(outputContainer))
+                    containerKindMap.put(outputContainer, new TIntHashSet());
+                final TIntSet kindSet = containerKindMap.get(outputContainer);
+                if (!kindSet.contains(kind)) {
+                    kindSet.add(kind);
+                    newRecipes.put(inputContainer.getMainEntry(furnaceRecipe.getKey().stackSize), outputContainer.getMainEntry(furnaceRecipe.getValue().stackSize));
+                } else
+                    furnaceRecipeIterator.remove();
+            }
+            furnaceRecipes.putAll(newRecipes);
+        }
     }
 }
--- src/main/java/wanion/unidict/integration/MekanismIntegration.java
+++ src/main/java/wanion/unidict/integration/MekanismIntegration.java
@@ -20,9 +20,11 @@ import net.minecraft.item.ItemStack;
 import wanion.unidict.Config;
 import wanion.unidict.MetaItem;
 import wanion.unidict.UniDict;
+import wanion.unidict.resource.UniResourceContainer;
 
 import javax.annotation.Nonnull;
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -49,23 +51,51 @@ final class MekanismIntegration extends AbstractIntegrationThread
     {
         final int initialSize = recipes.size();
         final Map<ItemStackInput, MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe>> correctRecipes = new HashMap<>(initialSize, 1);
-        final TIntSet uniques = new TIntHashSet(initialSize, 1);
-        for (Iterator<MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe>> mekanismRecipeIterator = recipes.values().iterator(); mekanismRecipeIterator.hasNext(); )
-        {
-            final MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe> mekanismRecipe = mekanismRecipeIterator.next();
-            final ItemStack correctOutput = resourceHandler.getMainItemStack(mekanismRecipe.recipeOutput.output);
-            if (correctOutput == mekanismRecipe.recipeOutput.output)
-                continue;
-            final MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe> newRecipe = mekanismRecipe.copy();
-            newRecipe.recipeOutput.output = correctOutput;
-            if (Config.keepOneEntry)
-                newRecipe.recipeInput.ingredient = resourceHandler.getMainItemStack(newRecipe.recipeInput.ingredient);
-            final int recipeID = MetaItem.getCumulative(newRecipe.recipeOutput.output, newRecipe.recipeInput.ingredient);
-            if (!uniques.contains(recipeID)) {
-                correctRecipes.put(newRecipe.recipeInput, newRecipe);
-                uniques.add(recipeID);
+        if (!(Config.keepOneEntry || Config.inputReplacement)) {
+            final TIntSet uniques = new TIntHashSet(initialSize, 1);
+            for (final Iterator<MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe>> mekanismRecipeIterator = recipes.values().iterator(); mekanismRecipeIterator.hasNext(); )
+            {
+                final MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe> mekanismRecipe = mekanismRecipeIterator.next();
+                final ItemStack correctOutput = resourceHandler.getMainItemStack(mekanismRecipe.recipeOutput.output);
+                if (correctOutput == mekanismRecipe.recipeOutput.output)
+                    continue;
+                final MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe> newRecipe = mekanismRecipe.copy();
+                newRecipe.recipeOutput.output = correctOutput;
+                if (Config.keepOneEntry)
+                    newRecipe.recipeInput.ingredient = resourceHandler.getMainItemStack(newRecipe.recipeInput.ingredient);
+                final int recipeID = MetaItem.getCumulative(newRecipe.recipeOutput.output, newRecipe.recipeInput.ingredient);
+                if (!uniques.contains(recipeID)) {
+                    correctRecipes.put(newRecipe.recipeInput, newRecipe);
+                    uniques.add(recipeID);
+                }
+                mekanismRecipeIterator.remove();
             }
-            mekanismRecipeIterator.remove();
+        } else {
+            final Map<UniResourceContainer, TIntSet> containerKindMap = new IdentityHashMap<>();
+            for (final Iterator<MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe>> mekanismRecipeIterator = recipes.values().iterator(); mekanismRecipeIterator.hasNext(); )
+            {
+                final MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe> mekanismRecipe = mekanismRecipeIterator.next();
+                final UniResourceContainer inputContainer = resourceHandler.getContainer(mekanismRecipe.recipeInput.ingredient);
+                final UniResourceContainer outputContainer = resourceHandler.getContainer(mekanismRecipe.recipeOutput.output);
+                if (outputContainer != null && inputContainer == null) {
+                    mekanismRecipe.recipeOutput.output = outputContainer.getMainEntry(mekanismRecipe.recipeOutput.output.stackSize);
+                    continue;
+                } else if (outputContainer == null)
+                    continue;
+                final int kind = inputContainer.kind;
+                if (!containerKindMap.containsKey(outputContainer))
+                    containerKindMap.put(outputContainer, new TIntHashSet());
+                final TIntSet kindSet = containerKindMap.get(outputContainer);
+                if (!kindSet.contains(kind)) {
+                    kindSet.add(kind);
+                    final MachineRecipe<ItemStackInput, ItemStackOutput, ? extends MachineRecipe> newRecipe = mekanismRecipe.copy();
+                    newRecipe.recipeOutput.output = outputContainer.getMainEntry(mekanismRecipe.recipeOutput.output.stackSize);
+                    if (Config.keepOneEntry || Config.inputReplacement)
+                        newRecipe.recipeInput.ingredient = inputContainer.getMainEntry(newRecipe.recipeInput.ingredient.stackSize);
+                    correctRecipes.put(newRecipe.recipeInput, newRecipe);
+                }
+                mekanismRecipeIterator.remove();
+            }
         }
         recipes.putAll(correctRecipes);
     }
@@ -74,23 +104,49 @@ final class MekanismIntegration extends AbstractIntegrationThread
     {
         final int initialSize = recipes.size();
         final Map<InfusionInput, MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe>> correctRecipes = new HashMap<>(initialSize, 1);
-        final TIntSet uniques = new TIntHashSet(initialSize, 1);
-        for (Iterator<MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe>> infusionRecipeIterator = recipes.values().iterator(); infusionRecipeIterator.hasNext(); )
-        {
-            final MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe> infusionRecipe = infusionRecipeIterator.next();
-            final ItemStack correctOutput = resourceHandler.getMainItemStack(infusionRecipe.recipeOutput.output);
-            if (correctOutput == infusionRecipe.recipeOutput.output)
-                continue;
-            final MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe> newRecipe = infusionRecipe.copy();
-            newRecipe.recipeOutput.output = correctOutput;
-            if (Config.keepOneEntry)
-                newRecipe.recipeInput.inputStack = resourceHandler.getMainItemStack(newRecipe.recipeInput.inputStack);
-            final int recipeID = MetaItem.getCumulative(newRecipe.recipeOutput.output, newRecipe.recipeInput.inputStack);
-            if (!uniques.contains(recipeID)) {
-                correctRecipes.put(newRecipe.recipeInput, newRecipe);
-                uniques.add(recipeID);
+        if (!(Config.keepOneEntry || Config.inputReplacement)) {
+            final TIntSet uniques = new TIntHashSet(initialSize, 1);
+
+            for (final Iterator<MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe>> infusionRecipeIterator = recipes.values().iterator(); infusionRecipeIterator.hasNext(); )
+            {
+                final MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe> infusionRecipe = infusionRecipeIterator.next();
+                final ItemStack correctOutput = resourceHandler.getMainItemStack(infusionRecipe.recipeOutput.output);
+                if (correctOutput == infusionRecipe.recipeOutput.output)
+                    continue;
+                final MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe> newRecipe = infusionRecipe.copy();
+                newRecipe.recipeOutput.output = correctOutput;
+                final int recipeID = MetaItem.getCumulative(newRecipe.recipeOutput.output, newRecipe.recipeInput.inputStack);
+                if (!uniques.contains(recipeID)) {
+                    correctRecipes.put(newRecipe.recipeInput, newRecipe);
+                    uniques.add(recipeID);
+                }
+                infusionRecipeIterator.remove();
             }
-            infusionRecipeIterator.remove();
+        }else {
+            final Map<UniResourceContainer, TIntSet> containerKindMap = new IdentityHashMap<>();
+            for (final Iterator<MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe>> infusionRecipeIterator = recipes.values().iterator(); infusionRecipeIterator.hasNext(); )
+            {
+                final MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe> mekanismRecipe = infusionRecipeIterator.next();
+                final UniResourceContainer inputContainer = resourceHandler.getContainer(mekanismRecipe.recipeInput.inputStack);
+                final UniResourceContainer outputContainer = resourceHandler.getContainer(mekanismRecipe.recipeOutput.output);
+                if (outputContainer != null && inputContainer == null) {
+                    mekanismRecipe.recipeOutput.output = outputContainer.getMainEntry(mekanismRecipe.recipeOutput.output.stackSize);
+                    continue;
+                } else if (outputContainer == null)
+                    continue;
+                final int kind = inputContainer.kind;
+                if (!containerKindMap.containsKey(outputContainer))
+                    containerKindMap.put(outputContainer, new TIntHashSet());
+                final TIntSet kindSet = containerKindMap.get(outputContainer);
+                if (!kindSet.contains(kind)) {
+                    kindSet.add(kind);
+                    final MachineRecipe<InfusionInput, ItemStackOutput, MetallurgicInfuserRecipe> newRecipe = mekanismRecipe.copy();
+                    newRecipe.recipeInput.inputStack = inputContainer.getMainEntry(newRecipe.recipeInput.inputStack.stackSize);
+                    newRecipe.recipeOutput.output = outputContainer.getMainEntry(mekanismRecipe.recipeOutput.output.stackSize);
+                    correctRecipes.put(newRecipe.recipeInput, newRecipe);
+                }
+                infusionRecipeIterator.remove();
+            }
         }
         recipes.putAll(correctRecipes);
     }
