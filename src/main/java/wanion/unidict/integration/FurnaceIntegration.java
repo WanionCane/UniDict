326cb67f5829db27b84639a61fe4b2ea0f4d7cd5
WanionCane <wanioncane@gmail.com>
2017-01-08 19:28:52 -0200

    1.10.2-2.5c

--- src/main/java/wanion/unidict/integration/FurnaceIntegration.java
+++ src/main/java/wanion/unidict/integration/FurnaceIntegration.java
@@ -16,6 +16,7 @@ import wanion.lib.common.Util;
 import wanion.unidict.UniDict;
 import wanion.unidict.resource.UniResourceContainer;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
@@ -28,18 +29,48 @@ final class FurnaceIntegration extends AbstractIntegrationThread
 		super("Furnace");
 	}
 
+	private static final Field experienceListField;
+
+	static {
+		Field obfuscatedField = null;
+		try {
+			obfuscatedField = FurnaceRecipes.class.getDeclaredField("field_77605_c");
+			obfuscatedField.setAccessible(true);
+		} catch (NoSuchFieldException e) {
+			UniDict.getLogger().debug("Dev-Environment, right?");
+		}
+		Field deobfuscatedField = null;
+		if (obfuscatedField == null) {
+			try {
+				deobfuscatedField = FurnaceRecipes.class.getDeclaredField("experienceList");
+				deobfuscatedField.setAccessible(true);
+			} catch (NoSuchFieldException e) {
+				UniDict.getLogger().error("UniDict couldn't find the experienceList Field, even you being in an Dev-Environment, please report this.");
+			}
+		}
+		experienceListField = obfuscatedField != null ? obfuscatedField : deobfuscatedField;
+	}
+
 	@Override
 	public String call()
 	{
 		try {
 			optimizeFurnaceRecipes();
-		} catch (Exception e) { UniDict.getLogger().error(threadName + e); }
+		} catch (Exception e) {
+			UniDict.getLogger().error(threadName + e);
+		}
 		return threadName + "Some things that you smelted appear to be different now.";
 	}
 
+	@SuppressWarnings("unchecked")
 	private void optimizeFurnaceRecipes()
 	{
-		final Map<ItemStack, Float> experienceMap = Util.getField(FurnaceRecipes.class, "experienceList", FurnaceRecipes.instance(), Map.class);
+		final Map<ItemStack, Float> experienceMap;
+		try {
+			experienceMap = (Map<ItemStack, Float>) experienceListField.get(FurnaceRecipes.instance());
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Furnace Integration Couldn't find the required field.");
+		}
 		if (!config.inputReplacementFurnace)
 			for (final Map.Entry<ItemStack, ItemStack> furnaceRecipe : FurnaceRecipes.instance().getSmeltingList().entrySet()) {
 				final ItemStack oldEntry = furnaceRecipe.getValue();
@@ -52,8 +83,7 @@ final class FurnaceIntegration extends AbstractIntegrationThread
 			final Map<UniResourceContainer, TIntSet> containerKindMap = new IdentityHashMap<>();
 			final Map<ItemStack, ItemStack> furnaceRecipes = FurnaceRecipes.instance().getSmeltingList();
 			final Map<ItemStack, ItemStack> newRecipes = new HashMap<>();
-			for (final Iterator<Map.Entry<ItemStack, ItemStack>> furnaceRecipeIterator = furnaceRecipes.entrySet().iterator(); furnaceRecipeIterator.hasNext(); )
-			{
+			for (final Iterator<Map.Entry<ItemStack, ItemStack>> furnaceRecipeIterator = furnaceRecipes.entrySet().iterator(); furnaceRecipeIterator.hasNext(); ) {
 				final Map.Entry<ItemStack, ItemStack> furnaceRecipe = furnaceRecipeIterator.next();
 				final UniResourceContainer inputContainer = resourceHandler.getContainer(furnaceRecipe.getKey());
 				final UniResourceContainer outputContainer = resourceHandler.getContainer(furnaceRecipe.getValue());
