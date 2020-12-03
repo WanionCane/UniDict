cf95fbd35825858bd593efcaa1468f92bf905b14
ElektroKill <skullmaster0103@gmail.com>
2020-12-03 14:20:34 +0100

    Fixed TechReborn Integration Updated to support the "praescriptum" recipes.

--- src/main/java/wanion/unidict/integration/TechRebornIntegration.java
+++ src/main/java/wanion/unidict/integration/TechRebornIntegration.java
@@ -8,16 +8,28 @@ package wanion.unidict.integration;
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import reborncore.api.praescriptum.recipes.Recipe;
 import reborncore.api.recipe.RecipeHandler;
 import techreborn.api.reactor.FusionReactorRecipe;
 import techreborn.api.reactor.FusionReactorRecipeHelper;
+import techreborn.api.recipe.Recipes;
 import wanion.lib.common.Util;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
 
 final class TechRebornIntegration extends AbstractIntegrationThread
 {
+	private Field recipeOutputs;
+
 	TechRebornIntegration()
 	{
 		super("TechReborn");
+		try {
+			(recipeOutputs = Recipe.class.getDeclaredField("itemOutputs")).setAccessible(true);
+		} catch (NoSuchFieldException | NullPointerException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -36,6 +48,31 @@ final class TechRebornIntegration extends AbstractIntegrationThread
 			if (!recipe.useOreDic())
 				resourceHandler.setMainObjects(recipe.getInputs());
 			resourceHandler.setMainItemStacks(recipe.getOutputs());
+		});
+
+		if (recipeOutputs == null)
+			return;
+
+		fixPraescriptumRecipes(Recipes.alloySmelter.getRecipes());
+		fixPraescriptumRecipes(Recipes.assemblingMachine.getRecipes());
+		fixPraescriptumRecipes(Recipes.centrifuge.getRecipes());
+		fixPraescriptumRecipes(Recipes.chemicalReactor.getRecipes());
+		fixPraescriptumRecipes(Recipes.compressor.getRecipes());
+		fixPraescriptumRecipes(Recipes.extractor.getRecipes());
+		fixPraescriptumRecipes(Recipes.grinder.getRecipes());
+		fixPraescriptumRecipes(Recipes.plateBendingMachine.getRecipes());
+		fixPraescriptumRecipes(Recipes.recycler.getRecipes());
+		fixPraescriptumRecipes(Recipes.solidCanningMachine.getRecipes());
+		fixPraescriptumRecipes(Recipes.wireMill.getRecipes());
+	}
+
+	private void fixPraescriptumRecipes(Collection<Recipe> recipes) {
+		recipes.forEach(recipe -> {
+			try {
+				recipeOutputs.set(recipe, resourceHandler.getMainItemStacks(recipe.getItemOutputs()));
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
 		});
 	}
 
